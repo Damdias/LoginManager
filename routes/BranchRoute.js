c6e3e67e5d6ac3e779e9248052ec065826a2f38c
Damith Dias <damith@Damiths-MacBook-Pro.local>
2018-04-13 16:44:36 +0530

    add branch create method

--- routes/BranchRoute.js
+++ routes/BranchRoute.js
@@ -1,11 +1,35 @@
 let errors = require("restify-errors");
-let User = require("../models/User");
+let Branch = require("../models/BranchModel");
 let _ = require("lodash");
 
 let branchRoutes = (server) => {
-    server.get('/branch', (req, res, next) => {
-        res.send({'name':'Malabe'});
-        next();
+    server.get('/branch/:id', (req, res, next) => {
+        let branch = new Branch();
+        branch.findOne({ _id: req.params.id }, (err, result) => {
+            if (err) {
+                next(new errors.InternalServerError(err));
+            }
+            else {
+                res.send(result);
+                next();
+            }
+        });
+    });
+    server.post('/branch', (req, res, next) => {
+        if (!req.is('application/json')) {
+            return next(
+                new errors.InvalidContentError("Expects 'applicaiton/json'")
+            );
+        }
+
+        let branch = new Branch(_.pick(req.body, ["branchName", "phoneNo", "address"]));
+        branch.save().then(() => {
+            res.status(201);
+            res.send();
+            next();
+        }).catch((err) => {
+            return next(new errors.InternalServerError(err));
+        })
     });
 }
 module.exports = branchRoutes;
