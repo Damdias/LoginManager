d68049297c821affb50df1a6acf9c6475982a05a
damith <damithasanka@gmail.com>
2018-05-30 03:31:38 +0530

    add aws config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || 'http://localhost:3000',
     db: {
-        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017' ,//'mongodb://127.0.0.1:27017/library',
+        uri: process.env.MONGODB_URI || 'mongodb://ec2-35-168-135-173.compute-1.amazonaws.com:27017' ,//'mongodb://127.0.0.1:27017/library',
         options:{
             dbName:process.env.MONGODB_DB || 'BNFM',
             user:process.env.MONGODB_USER || '',
