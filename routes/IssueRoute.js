7e818174db8f767abb16aca99762d367b9e993f9
damith <damithasanka@gmail.com>
2018-05-29 16:10:06 +0530

    add issue edit route

--- routes/IssueRoute.js
+++ routes/IssueRoute.js
@@ -11,14 +11,71 @@ let issueRoutes = (server) => {
                 new errors.InvalidContentError("Expects 'application/json'")
             );
         }
-
         let issue = new Issue(_.pick(req.body, ["title", 'description', 'branchId', 'branchName',
          'CreatedId', 'CreatedByName','AssignToUserId','AssignToUserName','images','Status',"IssueType"]));
-
         console.log("create issue",);
-        issue.save().then((user) => {
+        issue.save().then((issue) => {
             res.status(201);
-            res.send({ 'msg': 'User Created success' });
+            res.send({ 'msg': 'Issue Created success' });
+            next();
+
+        }).catch((e) => {
+            return next(new errors.InternalError(e.message));
+            next();
+        });
+
+    });
+    server.put('/issue', (req, res, next) => {
+
+        if (!req.is('application/json')) {
+            return next(
+                new errors.InvalidContentError("Expects 'application/json'")
+            );
+        }
+        let issuebody = _.pick(req.body, ["title", 'description', 'branchId', 'branchName',
+         'CreatedId', 'CreatedByName','AssignToUserId','AssignToUserName','images','Status',"IssueType"]);
+         Issue.findOne({ _id: req.body._id }).then((issue) => {
+            if (!issue) {
+                return next(new errors.InvalidArgumentError("Can't find Issue"))
+            }
+            issue.title = issuebody.title;
+            issue.description = issuebody.description;
+            issue.branchId = issuebody.branchId;
+            issue.branchName = issuebody.branchName;
+            issue.CreatedId = issuebody.CreatedId;
+            issue.CreatedByName = issuebody.CreatedByName;
+            issue.AssignToUserId = issuebody.AssignToUserId;
+            issue.AssignToUserName = issuebody.AssignToUserName;
+            issue.images = issuebody.images;
+            issue.Status = issuebody.Status;
+            issue.IssueType = issuebody.IssueType;
+            issue.save();
+            res.status(201);
+            res.send({ 'msg': 'Issue  update success' });
+            next();
+
+        }).catch((e) => {
+            return next(new errors.InternalError(e.message));
+            next();
+        });
+
+    });
+    server.put('/updateIssueStatus', (req, res, next) => {
+
+        if (!req.is('application/json')) {
+            return next(
+                new errors.InvalidContentError("Expects 'application/json'")
+            );
+        }
+        let issuebody = _.pick(req.body, ['Status']);
+         Issue.findOne({ _id: req.body._id }).then((issue) => {
+            if (!issue) {
+                return next(new errors.InvalidArgumentError("Can't find Issue"))
+            }
+            issue.IssueType = issuebody.IssueType;
+            issue.save();
+            res.status(201);
+            res.send({ 'msg': 'Issue  update success' });
             next();
 
         }).catch((e) => {
